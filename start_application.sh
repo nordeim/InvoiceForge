5860a6ff91c014a29ec0fad4afdc5bee743eb234
heinazhik <heinazhikweymong@outlook.com>
2025-12-15 11:07:27 +0800

    edit .env

--- start_application.sh
+++ start_application.sh
@@ -1,6 +1,12 @@
 # Option 1: With foreman
-foreman start -f Procfile.dev
+#foreman start -f Procfile.dev
 
+# Stop foreman (Ctrl+C) and restart:
+source .env && foreman start -f Procfile.dev
+
+exit 0
+
+###################################
 # Option 2: Two terminals
 # Terminal 1:
 npm run dev
